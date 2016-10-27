cade0c4654eec7efb21e2985fce19efb8e9bd9a3
Vungo0510 <hoanvu.ngo.2014@sis.smu.edu.sg>
2016-10-27 11:22:50 +0800

    Syntax and routing fix for library

--- test_5.2/app/Http/Controllers/LibraryController.php
+++ test_5.2/app/Http/Controllers/LibraryController.php
@@ -1,43 +1,41 @@
 <?php
-<<<<<<< HEAD
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
-
-class LibraryController extends Controller
+use App\Library;
+/*class LibraryController extends Controller
 {
     //$library = new Library();
 
     #showing individual student's library
     public function show_library(){
-		/*		 $a_library;
+				 $a_library;
         if ($student->email =="abc@gmail.com" && $student->deviceUID == "002724"){
             $books = $library->$get_books();
 						$a_library = $books;
             #return $books;
-        }*/
+        }
         echo 'this is lirary';
         //return \View::make('library', compact('a_library');
     }
 }
-=======
-namespace App\Http\Controllers;
+
+namespace App\Http\Controllers;*/
 
 use App\User;
-use App\Http\Controllers\Controller;
+//use App\Http\Controllers\Controller;
 
 
 class LibraryController extends Controller{
-private var $library = new Library();
 
     #showing individual student's library
-    public function $show_library($student){
+    public function show_library(Request $request){
+        $student = $request->user();
+        $library = new Library();
+        if ($student->email =="vn@smu.edu.sg"){ //replace this with the logged in username to check
+            $books = $library->get_books();
 
-        if ($student->email =="abc@gmail.com" && $student->deviceUID == "002724"){
-            $books = $library->$get_books();
+            echo "This is the library"; //to check if the app reaches here
 
             return $books;
         }
@@ -45,15 +43,16 @@ private var $library = new Library();
     }
 
     #add books to library
-    public function $addBook($book){
+    public function addBook($book){
+        $library = new Library();
         $book= $library->$add_book($book);
         return $book;
     }
     #set books
-    function $setBooks($books){
+    public function setBooks($books){
+        $library = new Library();
         $library->$set_books($books);
     }
 
+}
 
-?>
->>>>>>> origin/master
